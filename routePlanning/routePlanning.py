8e4d56f094ce6b9215a14ecaecb7d1be0373a56f
Dennis Chen PC <chendennis1998@gmail.com>
2019-07-15 11:56:39 +1000

    added random no fly zone generation

--- routePlanning/routePlanning.py
+++ routePlanning/routePlanning.py
@@ -1,5 +1,4 @@
 # TO DO: incorporate a list of waypoints that have to be followed
-
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -9,15 +8,18 @@ from descartes import PolygonPatch
 from shapely.geometry.polygon import LinearRing, Polygon
 from shapely.geometry import Point
 import shapely
-
-
 import time
+import random
+import math
 
+NO_FLY_ZONE_BOUNDARY_SIZE = 2
+WIDTH = 100
+HEIGHT = 99
 class routePlanning:
 
     def __init__(self):
-        self.height = 100 # hard coded values for size of the grid
-        self.width = 99
+        self.height = WIDTH # hard coded values for size of the grid
+        self.width = HEIGHT
         self.grid = np.ones((self.height, self.width))
         self.noFlyZones = []
         self.softNoFlyZones = []
@@ -38,7 +40,11 @@ class routePlanning:
             zonePatch.set_facecolor("orange")
             ax.add_patch(zonePatch)
 
-            softZonePoly = shapely.affinity.scale(zonePoly, xfact = 1.3, yfact = 1.3, zfact = 1.3, origin = 'centroid') # create soft no fly zones by 
+            # scaling based off size of desired boundary
+            # possible to implement this based off oby.boudns
+            radius = zonePoly.area ** 0.5
+            scalingFact = (radius + NO_FLY_ZONE_BOUNDARY_SIZE*2)/radius
+            softZonePoly = shapely.affinity.scale(zonePoly, xfact = scalingFact, yfact = scalingFact, zfact = scalingFact, origin = 'centroid') # create soft no fly zones by 
             self.softNoFlyZones.append(softZonePoly)
             softZonePatch = PolygonPatch(softZonePoly)
             softZonePatch.set_facecolor("orange")
@@ -51,6 +57,7 @@ class routePlanning:
         flyZonePatch.set_facecolor("green")
         flyZonePatch.set_alpha(0.2)
         ax.add_patch(flyZonePatch)
+        # plt.show()
 
         
 
@@ -68,6 +75,7 @@ class routePlanning:
         self.prevPositions.append(startPoint)
         ret = self.getNextMove(endPoint)
         if ret == 0:
+            plt.show()
             print("No path could be found")
             return
 
@@ -96,9 +104,10 @@ class routePlanning:
         # get the distance from end point 
         for x in range(currPos[0] - 1, currPos[0] + 2):
             for y in range(currPos[1] - 1, currPos[1] + 2): 
-                if x == self.prevPositions[0] and y == self.prevPositions[1]:
+                if x == self.prevPositions[0] and y == self.prevPositions[1] or (x,y) in self.prevPositions:
                     continue
                 possibleMovesAndDistance.append([(x,y), self.gridDistances[y][x]])
+                
         possibleMovesAndDistance.sort(key = lambda x:x[1]) # sort in ascending order
 
         # do the move which will get us closer to the endpoint
@@ -109,19 +118,50 @@ class routePlanning:
                 continue 
             else:
                 return 1
-
+        del self.prevPositions[-1]
         return 0 # means no path was viable
+
+def generateRandomNoFlyZones(n, size):
+    zones = []
+    for i in range(0,n):
+        x = random.randrange(0,WIDTH)
+        y = random.randrange(0,HEIGHT)
+        nSides = np.random.normal(5, 1.5)
+        nSides = round(nSides)
+        if nSides < 3:
+            nSides = 3
+        thetaStep = 2*3.14159/nSides
+        theta = np.random.normal(0, thetaStep/4)
+        zone = []
+        for j in range(0,nSides):
+            r = np.random.normal(size, size/2)
+            zone.append((x + r*math.cos(theta), y + r*math.sin(theta)))
+            theta = theta + np.random.normal(thetaStep, thetaStep/4)
+        zones.append(zone)
+    return zones
+
+
+
 
 if __name__ == "__main__":
 
     routePlanningObj = routePlanning()
-    flyZone = [(1,1),(70,10), (80,80), (5,90)]
-    noFlyZones = [
-        [(5,5), (6,15), (20,20), (9,5)],
-        [(40,40), (60,40), (60,60), (40,60)]
-
-    ]
+    flyZone = [(1,1),(80,5), (80,80), (5,90)]
+    noFlyZones = generateRandomNoFlyZones(5,6)
+    print(noFlyZones)
+    noFlyZones = [[(59.624414898112555, 81.40174607023728), (48.02314508819377, 86.59592378878118), (46.90299059872611, 71.39634435353503)], [(57.18027931634997, 38.29001668965869), (54.43530039767146, 45.63074538319422), (45.77066613734521, 42.756003445104355), (39.01808576676231, 44.92030294411203), (44.28385323609244, 40.08653747880057), (46.503672945158016, 36.38594001067716)], [(98.81836843413275, 72.93312811681439), (89.49513227037139, 78.85007976195699), (86.66596222903695, 66.90546112788763)], [(50.08481320568391, 35.72503611414005), (46.43885653343638, 39.31218583752805), (42.47651286590404, 32.59072582880941), (49.83985333606208, 32.69209160988164), (53.096603378125245, 35.713044556972235)], [(32.111308316264136, 40.172565718451814), (26.73136507047328, 42.835460210070885), (25.122587698977625, 42.703857181492054), (21.39047571307746, 46.172813386371416), (19.196345767308735, 43.05345996493098), (19.186660556856463, 35.237004304638816), (27.871139499357735, 32.43238753279634)]]
     routePlanningObj.createMap(flyZone, noFlyZones)
     routePlanningObj.planRoute((2,2), (79,79))
+
+"""
+    noFlyZones = [
+        [(5,7), (6,17), (20,22), (9,7)],
+        [(40,40), (60,40), (60,60), (40,60)],
+        [(20,40), (25,40), (25,45),(20,45)],
+        [(5,0), (10,0),(10,5),(5,5)],
+        [(-6, 5), (-1,5), (-1,10),(-5,10)]
+    ]
+    """
+    
 
 
